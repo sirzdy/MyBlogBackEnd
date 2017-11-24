6f26b07b3525adf4fb396b4dc9a2023428f69ef3
zdy <boxuerixin@gmail.com>
2017-11-24 18:11:19 +0800

    update

--- core/save.js
+++ core/save.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var multiparty = require('multiparty');
-const staticPath = __dirname + 'files/';
+const staticPath = __dirname + '/files/';
 const savePath = 'save/';
 const avatarPath = 'avatar/';
 var save = {
